4742443976441959e87b0442ca5f77f544a45230
pochiman <pochiman@yahoo.com>
2025-03-12 17:02:46 -0400

    initial commit

---
+++ 017_inserting_records_into_a_table.sql
@@ -0,0 +1,11 @@
+USE thriftshop;
+
+SELECT * FROM inventory;
+
+INSERT INTO inventory VALUES
+(15, 'item that will not exist', 2);
+
+INSERT INTO inventory VALUES
+(11, 'fur fox skin', 1),
+(12, 'plaid button up shirt', 8),
+(13, 'flannel zebra jammies', 6);
