284b022470234d9f76c686a7f320886d64438c28
pochiman <pochiman@yahoo.com>
2025-03-12 21:02:09 -0400

    initial commit

---
+++ 030_altering_tables.sql
@@ -0,0 +1,10 @@
+USE thriftshop;
+
+SELECT * FROM customer_purchases;
+
+ALTER TABLE customer_purchases
+DROP COLUMN customer_id;
+
+
+ALTER TABLE customer_purchases
+ADD COLUMN purchase_amount DECIMAL(10, 2) AFTER customer_purchase_id;
